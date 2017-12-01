c2a020242f6b17b156a5127ee5210c2bd46ae432
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-12-01 23:55:40 +0530

    chat working
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- public_static/index.js
+++ public_static/index.js
@@ -2,19 +2,58 @@
  * Created by rishabhkhanna on 30/11/17.
  */
 var socket = io();
+
 $(function () {
     var msg = $('#msg');
     var submit = $('#submit');
     var chat = $('#chat');
+    var online = $('#online');
 
     submit.click(function () {
         socket.emit('new_message', {
-         msg : msg.val()
+            username: username,
+            msg: msg.val()
         })
     });
 
+
+
     socket.on('get_msg', function (data) {
         console.log(data);
-        chat.append("<li>" + data.new_msg + "</li>")
+        chat.append(createMessage(data.username, data.new_msg))
     })
+
+    socket.on('prev', function(prev_msgs){
+        for (i of prev_msgs){
+            chat.append(createMessage(i.username, i.msg));
+
+        }
+    });
+
+    socket.on('users', function (users) {
+        for(i of users){
+            online.append(createOnlineList(i));
+        }
+    })
+
+    var username = prompt("Who you ?");
+
+    socket.emit('user', username);
+
 });
+
+function createMessage(user, msg) {
+    var msg = `<div class="list-group-item">
+                    <div class="d-flex w-40 justify-content-between">
+                        <h5 class="username">${user}</h5>
+                    </div>
+                    <p class="message">${msg}</p>
+                </div>`;
+    return msg;
+}
+
+function createOnlineList(user) {
+
+    var list = `<li class="list-group-item">${user}</li>`
+    return list;
+}
